2df9ab983a419d5faaa0d0f9ff124c70c8b26f2e
ddup2020 <v_xiaoping.gao@baidu.com>
2020-09-26 16:25:03 +0800

    更新博客地址

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # IOCDemo
 ioc 学习练手项目 <br/>
+博客地址：[手把手教你实现一个Butterknife](https://blog.csdn.net/sinat_41268473/article/details/108810898)
--- app/src/main/java/com/example/ioclearn/bean/bind/OnClick.java
+++ app/src/main/java/com/example/ioclearn/bean/bind/OnClick.java
@@ -7,6 +7,8 @@ import java.lang.annotation.Target;
 
 /**
  * Created by ddup on 2020/9/17.
+ * <p>
+ * 绑定onClick事件注解
  */
 @Retention(RetentionPolicy.RUNTIME) // 运行时注解
 @Target(ElementType.METHOD) // 注解类型是方法
